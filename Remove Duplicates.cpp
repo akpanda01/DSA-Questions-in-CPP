006fdedb3aa9c491d06bb00c5d7f401f8ded8a14
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2024-12-20 11:36:17 +0530

    Update Remove Duplicates.cpp

--- Remove Duplicates.cpp
+++ Remove Duplicates.cpp
@@ -16,3 +16,21 @@ public:
         return slow + 1; // Total unique elements
     }
 };
+
+//Another Method
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+        set < int > set;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            set.insert(arr[i]);
+        }
+        int k = set.size();
+        int j = 0;
+        for (int x: set) {
+            arr[j++] = x;
+        }
+        return k;
+    }
+};
